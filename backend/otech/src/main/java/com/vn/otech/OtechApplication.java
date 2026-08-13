cc1c7fd1d3420bb251e4749d5a112a3d43e1649b
tainguyen307 <23110307@student.hcmute.edu.vn>
2026-08-14 00:07:12 +0700

    Init project

---
+++ backend/otech/src/main/java/com/vn/otech/OtechApplication.java
@@ -0,0 +1,13 @@
+package com.vn.otech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtechApplication.class, args);
+	}
+
+}
---
+++ backend/otech/src/test/java/com/vn/otech/OtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.otech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
